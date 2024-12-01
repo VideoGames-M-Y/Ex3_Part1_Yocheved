5664e59d1f33a1ae9ccc5274aedf338243be98ae
Yocheved Ofstein <157640249+YochevedOfstein@users.noreply.github.com>
2024-12-01 20:08:52 +0200

    Update

--- Assets/Scripts/2-spawners/ClickSpawner.cs
+++ Assets/Scripts/2-spawners/ClickSpawner.cs
@@ -18,14 +18,11 @@ public class ClickSpawner: MonoBehaviour {
     }
 
     protected virtual GameObject spawnObject() {
-        //Debug.Log("Spawning a new object");
-
-        // Step 1: spawn the new object.
-        Vector3 positionOfSpawnedObject = transform.position;  // span at the containing object position.
-        Quaternion rotationOfSpawnedObject = Quaternion.identity;  // no rotation.
+        
+        Vector3 positionOfSpawnedObject = transform.position; 
+        Quaternion rotationOfSpawnedObject = Quaternion.identity; 
         GameObject newObject = Instantiate(prefabToSpawn, positionOfSpawnedObject, rotationOfSpawnedObject);
-
-        // Step 2: modify the velocity of the new object.
+        
         Mover newObjectMover = newObject.GetComponent<Mover>();
         if (newObjectMover) {
             newObjectMover.SetVelocity(velocityOfSpawnedObject);
